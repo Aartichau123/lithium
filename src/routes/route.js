adb25b3c5de17034041b310e01d7dd0b9beec742
AARTI CHAUHAN <96338623+itra420@users.noreply.github.com>
2022-10-21 13:56:07 +0530

    threeprobs

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,66 @@ const express = require('express');
 //const underscore = require('underscore')
 
 const router = express.Router();
-//consecative number problem
+
+let players=[{
+    "name": "manish",
+    "dob": "1/1/1995",
+    "gender": "male",
+    "city": "jalandhar",
+    "sports": [
+    "swimming"
+    ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+]
+   
+// players problems
+
+router.post('/players', function (req, res) {
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    //let player = players.find(p => p.name == newPlayersName)
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+    //undefined is same as false/ a falsy value
+    if (isNameRepeated) {
+        //Player exists
+        res.send("This player was already added!")
+    } else {
+        //New entry
+        players.push(newPlayer)
+        res.send(players)
+    }
+
+    })
+
+//consecative number problem 1
+
 // router.get('/sol1', function (req, res) {
 //     let arr=[1,2,3,5,6,7];
 //     let n=(arr[arr.length-1])
@@ -14,36 +73,26 @@ const router = express.Router();
 //     res.send({data:missingnumber})
 // })
 
-//non-consecative number problems
+//consecative number problems 2
+
 // router.get('/sol2', function (req, res) {
 //     let arr=[33,34,35,37,38];
-//     sumofArray = 0
-//     let first=[0]
-//     let last=(arr[arr.length-1])
+//     let missingNumber=0;
 //     for (let i=0;i<arr.length;i++){
-//         sumofArray+=arr[i]
+//         if(arr[i]+1!==arr[i+1]){  //a[0]+1 !=a[1]
+//             missingNumber=arr[i]+1;
+//             break;
+//         }
 //     }
-//     let n=arr.length+1
-//     let realsum=[n*(first+last)/2]
-//     let missingNumber =realsum-sumofArray
-//     console.log(sumofArray)
-//     console.log(realsum)
-//     console.log(last)
 //     console.log(missingNumber)
 //     res.send({data:missingNumber})
 // })
 
-router.get('/sol2', function (req, res) {
-    let arr=[33,34,35,37,38];
-    let missingNumber=0;
-    for (let i=0;i<arr.length;i++){
-        if(arr[i]+1!==arr[i+1]){
-            missingNumber=arr[i]+1;
-            break;
-        }
-    }
-    console.log(missingNumber)
-    res.send({data:missingNumber})
-});
+
+// 
+
+
+
 module.exports = router;
-// adding this comment for no reason
+
+// adding this comment for no reasonnpx
